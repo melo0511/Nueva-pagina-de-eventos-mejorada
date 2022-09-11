e16456d8591eecfea13884f0faed06e247808731
Jhonatan Melo <josejhonatanxd@gmail.com>
2022-09-11 12:42:19 -0500

    Agregado el boton de cambio de color, pero no funciona por error de addEventlistener en consola

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btnMode');
+
+const header = document.getElementById('header');
+
+btn.addEventListener('click',()=>{
+header.style.backgroundColor = '#000000'
+})
